a5c51cf6a88ca7373e021c701ba548a9dd2440ca
Thibault Anani <thuny.ta@gmail.com>
2025-09-21 23:05:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tide-feature-selection',
-    version='1.1.5',
+    version='1.2.0',
     packages=find_packages(),
     install_requires=[
         'numpy',
